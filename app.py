c6b8d54df7cca675b3ee2a32bf0f7b0b393f7878
matias_cs <matias.mena@comunidad.ub.edu.ar>
2025-05-29 14:50:24 -0300

    Added logger and utils

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from books_recommender.exception.exception_handler import AppException
+from books_recommender.logger.log import logging
 
 print("\n--- Diagnostic: sys.path from app.py ---")
 for i, p in enumerate(sys.path):
@@ -16,4 +17,5 @@ print("-------------------------------------------\n")
 try:
     a = 3/0
 except Exception as e:
+    logging.info()
     raise AppException(e,sys) from e
